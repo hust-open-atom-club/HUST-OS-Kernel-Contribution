8408ccd56ae38f42321b399d56194f891a64cc6d
Dongliang Mu <dzm91@hust.edu.cn>
2024-02-22 16:02:30 +0800

    update linux-next deploy script

--- linux-next/deploy_smatch_on_linux-next.sh
+++ linux-next/deploy_smatch_on_linux-next.sh
@@ -6,6 +6,7 @@ REPO_NAME="linux-next-master"
 TESTING_KERNEL="${REPO_NAME}.tar.gz"
 DOWNLOAD_URL="https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/snapshot/linux-next-master.tar.gz"
 
+# cleanup old kernel tree and smatch instance
 if [ -d ${REPO_NAME} ]; then
     /bin/rm -rf ${REPO_NAME}
 fi
@@ -14,6 +15,7 @@ if [ -d smatch ]; then
     /bin/rm -rf smatch/
 fi
 
+# download the corresponding kernel tree
 wget $DOWNLOAD_URL -O ${TESTING_KERNEL}
 
 # simply check file format
@@ -25,23 +27,30 @@ else
     exit -1;
 fi
 
+# copy smatch into current folder (TODO: git archive to get compressed smatch)
 cp -r ../smatch ${PWD}/
 
+# build smatch
 pushd smatch
 make -j8
 popd
 
 pushd ${REPO_NAME}
+
 # if allyesconfig does not work, choose to an existing config
 if [ -e ../config ]; then
 	cp ../config .config
-	make oldconfig
+	yes "" | make oldconfig
 	cp .config ../config
 else
 	make allyesconfig
 fi
 
+# build cross-function database and test in the whole kernel
 ../smatch/smatch_scripts/build_kernel_data.sh
 ../smatch/smatch_scripts/test_kernel.sh
 #../smatch/smatch_scripts/kchecker drivers/net/wireless
+
+# copy smatch_warns.txt outside 
+cp smatch_warns.txt ../
 popd
